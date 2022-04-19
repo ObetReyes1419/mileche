364f106fc422ded8656f4ce1f24d1b063302b735
obetreyes1419 <obetreyes1419@gmail.com>
2022-04-18 20:33:37 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return(
+           <nav className="pa3 pa4-ns">
+  <a className="link dim black b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home">mi leche</a>
+  <div className="tc pb3">
+    <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Inicio</a>
+    <a className="link dim gray f6 f5-ns dib mr3" href="#" title="About">Tienda</a>
+    <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Nosotros</a>
+  </div>
+</nav>
+
+    )
+}
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+ const Hero = () => {
+  return (
+    <section class="flex-ns vh-100 items-center">
+    <div class="mw6 ph5">
+      <img src="http://tachyons.io/img/iPhone7Vertical.jpg"/> 
+    </div>
+    <div class="tc tl-ns ph3">
+      <h1 class="f3 f1-l fw2 mb3 mt4 mt0-ns">Connect in a whole new way.</h1>
+      <h2 class="f5 f3-l fw1 mb4 mb5-l lh-title">Rated the #1 app for communicating with customers.</h2>
+
+
+    </div>
+  </section>
+  
+  
+  
+  )
+}
+
+export default Hero;
